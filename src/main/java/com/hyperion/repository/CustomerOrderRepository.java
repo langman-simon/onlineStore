031c8b0a18730cdb6e4a2a12041bf167588ea6cd
langman-simon <langmansimon@hotmail.com>
2026-08-02 15:07:15 +0200

    add order listing as interface method

--- src/main/java/com/hyperion/repository/CustomerOrderRepository.java
+++ src/main/java/com/hyperion/repository/CustomerOrderRepository.java
@@ -3,6 +3,12 @@ package com.hyperion.repository;
 import com.hyperion.model.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerOrderRepository
         extends JpaRepository<CustomerOrder, Long> {
+
+    List<CustomerOrder> findByUserLoginOrderByCreatedAtDesc(
+            String login
+    );
 }
